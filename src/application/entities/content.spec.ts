7a556bf6419df57e649e51c671578a7e438dc0b7
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-12-29 17:05:57 -0300

    updates unit test for content

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -9,11 +9,11 @@ describe('Notification content', () =>{
         expect(content).toBeTruthy();
     });
       
-    it('should be able to create a notification content', () => {  
+    it('should not be able to create a notification content with less than 5 characters', () => {  
           expect(() => new Content('aaa')).toThrow();
     });
       
-    it('should be able to create a notification content', () => {  
+    it('should not be able to create a notification content with more than 240 characters', () => {  
           expect(() => new Content('a'.repeat(241))).toThrow();
     });
 });
