c6e90c75a2e757f17f4df76c2802d7d80910a55a
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-12-29 19:47:53 -0300

    creates the unit test for sending notifications

--- src/application/entities/use-cases/send-notification.spec.ts
+++ src/application/entities/use-cases/send-notification.spec.ts
@@ -1,17 +1,29 @@
 /* eslint-disable prettier/prettier */
+import { Notification } from "../notification";
 import { SendNotification } from "./send-notification"
+
+
+const notifications: Notification[]=[];
+
+const notificationRepository = {
+    async create(notification:Notification){
+        notifications.push(notification)
+    }
+}
 
 describe('Send notification', () =>{
 
     it('should send a notification',async () =>{
-        const sendNotification = new SendNotification();
+        const sendNotification = new SendNotification(notificationRepository);
 
-        const {notification} = await sendNotification.execute({
+         await sendNotification.execute({
             content: 'This is a notification',
             category: 'social',
             recipientId:'example-recipient-id'
         });
 
-        expect(notification).toBeTruthy();
+        console.log(notifications)
+
+        expect(notifications).toHaveLength(1);
     });
 });
