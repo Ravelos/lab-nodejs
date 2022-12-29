b80fc003324df915f7ee8c2bbb665968ece3a83f
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-12-29 19:49:13 -0300

    adds the unit test for notification service

--- src/application/entities/notification.spec.ts
+++ src/application/entities/notification.spec.ts
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Content } from './content';
-import { Notificacion } from './notification';
+import { Notification } from './notification';
 
 describe('Notification', () => {
   it('should be able to create a notification', () => {
-    const notification = new Notificacion({
+    const notification = new Notification({
         content: new Content('You have a new friendship request'),
         recipientId: 'reciepient-id-example',
         category: 'social',
