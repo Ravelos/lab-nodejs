e715facea70c9f209f2085c74ad437d08653431a
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-14 11:56:19 -0300

    adjusts the name for this file

--- src/application/repositories/notification-repository.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-
-import { Notification } from "../entities/notification";
-
-export abstract class NotificationRespository{
-    abstract create(notification: Notification): Promise<void>;
-}
