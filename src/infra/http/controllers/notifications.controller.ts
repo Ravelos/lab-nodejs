7a1142589e20b1dcfdeebb8dbb0811f1f32550bb
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-14 14:26:21 -0300

    creates the folder for controllers

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from 'src/application/entities/use-cases/send-notification';
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification){}
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    const {notification} = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return {notification};
+  }
+}
