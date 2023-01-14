c78fb5932aaa084673915393ee6483a5003712f7
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-14 14:25:15 -0300

    creates in memory repository for testing purpose

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { NotificationRespository } from '../../src/application/repositories/notifications-repository';
+import { NotificationsRespository } from '../../src/application/repositories/notifications-repository';
 import { Notification } from '../../src/application/entities/notification';
 
-export class InMemoryNotificationsRepository implements NotificationRespository {
+export class InMemoryNotificationsRepository implements NotificationsRespository {
   public notifications: Notification[] = [];
 
   async create(notification: Notification) {
