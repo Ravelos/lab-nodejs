dabb4fd67e564585263f8a02f3038d67b7f3879a
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-19 21:48:52 -0300

    creates mappers folder

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Notification } from 'src/application/entities/notification';
+
+export class PrismaNotificationMappers {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      category: notification.category,
+      content: notification.content.value,
+      recipientId: notification.recipientId,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
+  }
+}
