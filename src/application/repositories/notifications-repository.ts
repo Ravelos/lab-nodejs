7c7079c29eb45538f3ff87f19b24ff20f33689ff
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-20 21:08:55 -0300

    creates the abstract method for cancelling notifications

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -4,4 +4,5 @@ import { Notification } from "../entities/notification";
 
 export abstract class NotificationsRespository{
     abstract create(notification: Notification): Promise<void>;
+    abstract findById(notificationId:string): Promise<Notification | null>;
 }
