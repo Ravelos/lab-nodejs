e9c5a023789eb439563b7f68d8fde7bb3de98b22
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-28 19:50:44 -0300

    adjusts in memory notifications repository

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -3,6 +3,10 @@ import { NotificationsRespository } from '../../src/application/repositories/not
 import { Notification } from '../../src/application/entities/notification';
 
 export class InMemoryNotificationsRepository implements NotificationsRespository {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
   public notifications: Notification[] = [];
 
   async create(notification: Notification) {
