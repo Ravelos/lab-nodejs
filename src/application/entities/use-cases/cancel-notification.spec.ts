e3f5bc5cf191a5d44d4facaba83132625493c84a
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-28 19:49:39 -0300

    creates the unit test for cancel notification

---
+++ src/application/entities/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { InMemoryNotificationsRepository } from '../../../../test/repositories/in-memory-notifications-repository';
+import { Content } from '../content';
+import { Notification } from '../notification';
+import { CancelNotification } from './cancel-notification';
+
+
+
+describe('Cancel notification', () =>{
+
+    it('should cancel a notification',async () =>{
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+        const notification = new Notification({
+            category:'social',
+            content: new Content('Hello! Test'),
+            recipientId: 'example-recipient-id',
+            createdAt: new Date()
+        })
+
+        await notificationsRepository.create(notification);
+
+       await cancelNotification.execute({
+        notificationId: notification.id
+        });
+
+        expect(notificationsRepository.notifications[0].canceleAt).toBeTruthy;
+    });
+});
