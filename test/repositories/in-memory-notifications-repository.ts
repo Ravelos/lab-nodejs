ab43a11e5532013ee676df86dd54cfaa09c81144
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-01-28 20:29:38 -0300

    implement all the methods

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -5,11 +5,27 @@ import { Notification } from '../../src/application/entities/notification';
 export class InMemoryNotificationsRepository implements NotificationsRespository {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async findById(notificationId: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
+    const notification = this.notifications.find((item) => item.id === notificationId);
+    if (!notification){
+      return null;
+    }
+    {
+      return notification;
+    }
   }
   public notifications: Notification[] = [];
 
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification:Notification): Promise<void>{
+    const notificationIndex = this.notifications.findIndex(
+      item => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0){
+      this.notifications[notificationIndex] = notification;
+    }
   }
 }
