69fe5b37ba04060ad00aa92da9a0bee26e55ed52
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-05 18:51:00 -0300

    shows the logs from the response

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -1,8 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+  constructor(){
+    super({
+      log: ['query']
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
