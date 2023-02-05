78cba2579b79dc59c47baf7007a760e0a4de8685
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-05 15:47:43 -0300

    creates the static domain function

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+import {Notification as RawNotification} from '@prisma/client'
+import { Content } from 'src/application/entities/content';
 import { Notification } from 'src/application/entities/notification';
 
 export class PrismaNotificationMappers {
@@ -11,5 +13,19 @@ export class PrismaNotificationMappers {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification{
+    return new Notification({
+      category: raw.category,
+      content: new Content(raw.content),
+      recipientId: raw.recipientId,
+      readAt: raw.readAt,
+      canceledAt: raw.canceledAt,
+      createdAt: raw.createdAt
+    }, 
+    raw.id
+    );
+
   }
 }
