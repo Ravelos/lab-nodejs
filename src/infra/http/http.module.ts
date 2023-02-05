1bd72dc3ce235e52462fa2f2362c563b8e71ac62
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-05 18:52:49 -0300

    imports the providers

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { CancelNotification } from 'src/application/entities/use-cases/cancel-notification';
+import { CountRecipientNotification } from 'src/application/entities/use-cases/count-recipient-notifications';
+import { GetRecipientNotification } from 'src/application/entities/use-cases/get-recipient-notifications';
+import { ReadNotification } from 'src/application/entities/use-cases/read-notification';
+import { UnreadNotification } from 'src/application/entities/use-cases/unread-notification';
 import { SendNotification } from '../../application/entities/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
@@ -7,6 +12,13 @@ import { NotificationsController } from './controllers/notifications.controller'
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+    GetRecipientNotification,
+    CountRecipientNotification
+  ],
 })
 export class HttpModule {}
